1ba6920495dcaa35fdd98c4ecef15e8a776f058b
thaivc <giacatluong20121996@gmail.com>
2024-05-20 21:27:25 +0700

    :fire: init project

---
+++ src/main/java/gc/garcol/simplequadtreejava/SimpleQuadtreeJavaApplication.java
@@ -0,0 +1,13 @@
+package gc.garcol.simplequadtreejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleQuadtreeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleQuadtreeJavaApplication.class, args);
+    }
+
+}
